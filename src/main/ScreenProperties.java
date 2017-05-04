334d11194205461c20b764c10d9fc06d1dc4fae3
foepje12 <jellevanommen@hotmail.nl>
2017-05-04 10:41:26 +0200

    Initial Commit

---
+++ src/main/ScreenProperties.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+public class ScreenProperties
+{
+	private static GraphicsDevice gd;
+
+	public static int getScreenWidth()
+	{
+		gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		return gd.getDisplayMode().getWidth();
+	}
+
+	public static int getScreenHeight()
+	{
+		gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		return gd.getDisplayMode().getHeight();
+	}
+}
