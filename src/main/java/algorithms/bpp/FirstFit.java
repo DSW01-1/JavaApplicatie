0fa1fd6001f46a9110c5193909a2f131b1aba3d8
Sander <sanderhagen@outlook.com>
2017-05-10 12:47:25 +0200

    firstFit updated

--- src/main/java/algorithms/bpp/FirstFit.java
+++ src/main/java/algorithms/bpp/FirstFit.java
@@ -1,17 +1,30 @@
-package main.java.algorithms;
+package main.java.algorithms.bpp;
 import main.java.graphs.Box;
 import main.java.graphs.Product;
 import java.util.ArrayList;
 
 
 public class FirstFit {
-    private int boxAmount;
-    private Box currentBox = new Box();
-    public FirstFit(ArrayList<Product> producten){
-        for(Product product: producten){
-            if(currentBox.checkFit(product.getSize())){
+    private ArrayList<Box> returnBoxen = new ArrayList();
+    private Box firstBox = new Box();
+    private boolean fitted = false;
 
+    public ArrayList<Box> executeFirstFit(ArrayList<Product> products) {
+        returnBoxen.add(firstBox);
+        for (Product product : products) {
+            for (Box currentBox : returnBoxen) {
+                if (currentBox.checkFit(product.GetSize())&&fitted==false) {
+                    currentBox.addProduct(product);
+                    fitted = true;
+                }
             }
+            if (fitted == false) {
+                Box newBox = new Box();
+                returnBoxen.add(newBox);
+                newBox.addProduct(product);
+            }
+            fitted = false;
         }
+        return returnBoxen;
     }
 }
