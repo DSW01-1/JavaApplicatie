02696deb7177ed6942a4fa86ec5787fc3d3b1223
Sander <sanderhagen@outlook.com>
2017-05-10 12:16:51 +0200

    overbodige variabele removed

--- src/main/java/graphs/Box.java
+++ src/main/java/graphs/Box.java
@@ -5,16 +5,14 @@ import java.util.ArrayList;
 public class Box {
 
     private int volume = 10;
-    private ArrayList<Product> products= new ArrayList();
-    //x is location in contents array
-    int x = 0;
+    private ArrayList<Product> products= new ArrayList<Product>();
 
     public boolean checkFit(int size){
         return size < volume;
     }
 
     public void addProduct(Product product){
-        //geen check of hij past omdat dat al in het algoritme gebeurt.
+        //not necessary to check fit, done in algorithm
         this.products.add(product);
         this.volume -= product.GetSize();
     }
