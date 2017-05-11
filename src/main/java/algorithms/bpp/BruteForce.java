57a58b4c5214abe12d594a40e23366f34988f4df
Sander <sanderhagen@outlook.com>
2017-05-11 14:20:00 +0200

    slightly improved bruteForce, Bugmasters version

--- src/main/java/algorithms/bpp/BruteForce.java
+++ src/main/java/algorithms/bpp/BruteForce.java
@@ -7,39 +7,38 @@ import java.util.Collections;
 
 public class BruteForce
 {
+	//returnBoxes is de arrayList die boxen returned met de producten.
+	//latestBoxes is de laatste gesorteerde arraylist
 	private ArrayList<Box> returnBoxes = new ArrayList<Box>();
 	private ArrayList<Box> latestBoxes = new ArrayList<Box>();
-	private int leastBoxAmount = 1000;
 	private boolean fitted;
 	private int x;
 	public ArrayList<Box> executeBruteForce(ArrayList<Product> products) {
-		for (Product product : products) {
-			for (Box currentBox : latestBoxes) {
-				if (currentBox.checkFit(product.GetSize()) && fitted == false) {
-					currentBox.addProduct(product);
-					fitted = true;
+		while(x<products.size()-1) {
+			for (Product product : products) {
+				for (Box currentBox : latestBoxes) {
+					if (currentBox.checkFit(product.GetSize()) && fitted == false) {
+						currentBox.addProduct(product);
+						fitted = true;
+					}
 				}
+				if (!fitted) {
+					Box currentBox = new Box(10);
+					currentBox.addProduct(product);
+					latestBoxes.add(currentBox);
+				}
+				fitted = false;
 			}
-			if (!fitted) {
-				Box currentBox = new Box(10);
-				currentBox.addProduct(product);
-				latestBoxes.add(currentBox);
+			Collections.rotate(products, 1);
+			x++;
+			if (latestBoxes.size() < returnBoxes.size()||returnBoxes.size()==0) {
+				System.out.println(latestBoxes.size());
+				returnBoxes = latestBoxes;
+				System.out.println(returnBoxes.size());
 			}
-			fitted = false;
 		}
-		if (latestBoxes.size() < leastBoxAmount) {
-			leastBoxAmount = latestBoxes.size();
-			returnBoxes = latestBoxes;
-		}
-		Collections.rotate(products, 1);
 		System.out.println(returnBoxes);
 		System.out.println(returnBoxes.size() + " box(es) needed");
 		return returnBoxes;
-	}
-
-	public void clearHistory(){
-		returnBoxes.clear();
-		latestBoxes.clear();
-		leastBoxAmount=1000;
 	}
 }
