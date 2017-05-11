50c251154e075c0e52ca25c4d01e729f63ae8865
Auke <auke.v.o@hotmail.com>
2017-05-11 09:54:09 +0200

    Quick fix | Nearest neighbour

--- src/main/java/algorithms/tsp/NearestNeighbour.java
+++ src/main/java/algorithms/tsp/NearestNeighbour.java
@@ -23,163 +23,145 @@ public class NearestNeighbour
 		int indexFirstTile = findFirstTile();
 	}
 
-<<<<<<< HEAD
     private int findFirstTile(){
         int index = 0;
         int x = 0;
         int y = 0;
         boolean found = false;
         int step = 0;
-=======
-	private int findFirstTile()
-	{
-		int index = 0;
-		int x = 0;
-		int y = 0;
-		boolean found = false;
->>>>>>> 2e0a5f4a7f45648916f1e0242914f5f145b491a5
 
 		simulatie.addConsoleItem("Searching for first coordinate", "DEBUG");
 		simulatie.addConsoleItem("Moving to first coordinate (0, 0)", "DEBUG");
 
-<<<<<<< HEAD
-        simulatie.addConsoleItem(String.format("=====| LOOP THROUGH ARRAY |====="), "DEBUG");
-        int test = 1;
-        for(GridTile tile : CoordList){
-            simulatie.addConsoleItem(String.format("tile %s: x=%s, y=%s", test, tile.getXcoord(), tile.getYcoord()), "DEBUG");
-            test++;
-        }
-        simulatie.addConsoleItem(String.format("=====| END OF LOOP |====="), "DEBUG");
 
-
-        while (!found){
+        //while (!found){
             //String cnt = (i < 9) ? String.format("0%d",i+1) : String.format("%d",i+1) ;
+        //    simulatie.addConsoleItem(String.format("=====| NEXT COORDINATE (x: %s ,y: %s)",x,y), "DEBUG");
 
+        //    int i = 0;
 
-            simulatie.addConsoleItem(String.format("=====| NEXT COORDINATE (x: %s ,y: %s)",x,y), "DEBUG");
+            /* ===== COORDLIST LOOP 1 ===== */
+            //for(GridTile tile : CoordList){
+            //    boolean xOk = (tile.getXcoord() == x);
+            //    boolean yOk = (tile.getYcoord() == y);
 
-            int i = 0;
+            //    System.out.printf("%s - %s \n", tile.getXcoord(), tile.getYcoord());
 
-            // loop through coordlist
-            for(GridTile tile : CoordList){
-                boolean xOk = (tile.getXcoord() == x);
-                boolean yOk = (tile.getYcoord() == y);
-
-                System.out.printf("%s - %s \n", tile.getXcoord(), tile.getYcoord());
-
-                if(xOk && yOk){
-                    found = true;
-                    simulatie.addConsoleItem(String.format("Tile %s is correct!, moving to the next step",i), "DEBUG");
-                    //index = i;
-                    //break;
-                }else{
-                    simulatie.addConsoleItem(String.format("Tile %s is incorrect!,",i), "DEBUG");
-                }
-                i++;
-            }
-
-            // lozde looop
-            if(!found){
-                if(step < (Constants.gridSize * 2) ){
-                    if(y > 0 && x < (Constants.gridSize - 1)){
-                        y--;
-                        x++;
-                    }else{
-                        if(step < (Constants.gridSize - 1) ){  // 1e 5 stappen
-                            step++;
-                            y = step;//Constants.gridSize - 1;
-                            x = 0;//step - (Constants.gridSize - 1);
-                        }else{  // 2e 5 stappen
-                            step++;
-                            y = (Constants.gridSize - 1);
-                            x = step - (Constants.gridSize - 1);
-                        }
-                    }
-                }else{
-                    found = true;
-                }
-            }
-
-            /*
-            for(GridTile tile : CoordList){
-                boolean xOk = (tile.getXcoord() == x);
-                boolean yOk = (tile.getYcoord() == y);
-=======
-		while (!found)
-		{
-			int i = 0;
-			for (GridTile tile : CoordList)
-			{
-				boolean xOk = (tile.getXcoord() == x);
-				boolean yOk = (tile.getYcoord() == y);
->>>>>>> 2e0a5f4a7f45648916f1e0242914f5f145b491a5
-
-				System.out.printf("%s - %s \n", tile.getXcoord(), tile.getYcoord());
-
-<<<<<<< HEAD
-                if(xOk && yOk){
-                    found = true;
-                    simulatie.addConsoleItem(String.format("Coordinate %s is correct!, moving to the next step",i), "DEBUG");
-                    //index = i;
-                    break;
-                }else{
-                    simulatie.addConsoleItem(String.format("Coordinate %s is incorrect!, moving on",i), "DEBUG");
-                }
-                i++;
-            }*/
-            //if(!found){
-
-                /*
-                if(x == y && x < Constants.gridSize){
-                    x++;
-                    simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
-                }else if(y < Constants.gridSize){
-                    y++;
-                    simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
-                }else{
-                    simulatie.addConsoleItem("Coordinate not found", "DEBUG");
-                    found = true;
-                }
-                */
+            //    if(xOk && yOk){
+            //        found = true;
+            //        simulatie.addConsoleItem(String.format("Tile %s is correct!, moving to the next step",i), "DEBUG");
+            //        //index = i;
+            //        //break;
+            //    }else{
+            //        simulatie.addConsoleItem(String.format("Tile %s is incorrect!,",i), "DEBUG");
+            //    }
+            //    i++;
             //}
-        }
-=======
-				if (xOk && yOk)
-				{
-					found = true;
-					simulatie.addConsoleItem(String.format("Coordinate %s is correct!, moving to the next step", i),
-							"DEBUG");
-					// index = i;
-					break;
-				}
-				else
-				{
-					simulatie.addConsoleItem(String.format("Coordinate %s is incorrect!, moving on", i), "DEBUG");
-				}
-				i++;
-			}
-			if (!found)
-			{
-				if (x == y && x < Constants.gridSize)
-				{
-					x++;
-					simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
-				}
-				else if (y < Constants.gridSize)
-				{
-					y++;
-					simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
-				}
-				else
-				{
-					simulatie.addConsoleItem("Coordinate not found", "DEBUG");
-					found = true;
-				}
-			}
 
-		}
->>>>>>> 2e0a5f4a7f45648916f1e0242914f5f145b491a5
+            /* ===== LOOP 1 ===== */
+            // lozde looop
+            //  if(!found){
+            //    if(step < (Constants.gridSize * 2) ){
+            //        if(y > 0 && x < (Constants.gridSize - 1)){
+            //            y--;
+            //            x++;
+            //        }else{
+            //            if(step < (Constants.gridSize - 1) ){  // 1e 5 stappen
+            //                step++;
+            //                y = step;//Constants.gridSize - 1;
+            //                x = 0;//step - (Constants.gridSize - 1);
+            //            }else{  // 2e 5 stappen
+            //                step++;
+            //                y = (Constants.gridSize - 1);
+            //                x = step - (Constants.gridSize - 1);
+            //            }
+            //        }
+            //    }else{
+            //        found = true;
+            //    }
+            //}
 
+
+            //for(GridTile tile : CoordList){
+            //    boolean xOk = (tile.getXcoord() == x);
+            //    boolean yOk = (tile.getYcoord() == y);
+
+			//	if (xOk && yOk)
+			//	{
+			//		found = true;
+			//		simulatie.addConsoleItem(String.format("Coordinate %s is correct!, moving to the next step", i),
+			//				"DEBUG");
+			//		// index = i;
+			//		break;
+			//	}
+			//	else
+			//	{
+			//		simulatie.addConsoleItem(String.format("Coordinate %s is incorrect!, moving on", i), "DEBUG");
+			//	}
+			//	i++;
+			//}
+
+
+			//if (!found)
+			//{
+			//	if (x == y && x < Constants.gridSize)
+			//	{
+			//		x++;
+			//		simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
+			//	}
+			//	else if (y < Constants.gridSize)
+			//	{
+			//		y++;
+			//		simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
+			//	}
+			//	else
+			//	{
+			//		simulatie.addConsoleItem("Coordinate not found", "DEBUG");
+			//		found = true;
+			//	}
+			//}
+
+
+
+			//while (!found)
+			//{
+			//	int i = 0;
+			//	for (GridTile tile : CoordList)
+			//	{
+			//		boolean xOk = (tile.getXcoord() == x);
+			//		boolean yOk = (tile.getYcoord() == y);
+//
+//					System.out.printf("%s - %s \n", tile.getXcoord(), tile.getYcoord());
+//
+//					if(xOk && yOk){
+//						found = true;
+//						simulatie.addConsoleItem(String.format("Coordinate %s is correct!, moving to the next step",i), "DEBUG");
+//						//index = i;
+//						break;
+//					}else{
+//						simulatie.addConsoleItem(String.format("Coordinate %s is incorrect!, moving on",i), "DEBUG");
+//					}
+//					i++;
+//				}*/
+//				//if(!found){
+//
+//					/*
+//					if(x == y && x < Constants.gridSize){
+//						x++;
+//						simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
+//					}else if(y < Constants.gridSize){
+//						y++;
+//						simulatie.addConsoleItem(String.format("Moving to new Coordinate, (%s, %s)", x, y), "DEBUG");
+//					}else{
+//						simulatie.addConsoleItem("Coordinate not found", "DEBUG");
+//						found = true;
+//					}
+//					*/
+//				//}
+//			}
+
+
+		//}
 		return index;
 	}
 }
