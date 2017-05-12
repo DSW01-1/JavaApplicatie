12733852490a8cf997088d1ef155ec1f41e7705c
foepje12 <jellevanommen@hotmail.nl>
2017-05-12 18:07:53 +0200

    compiler error

--- src/main/java/algorithms/bpp/BruteForce.java
+++ src/main/java/algorithms/bpp/BruteForce.java
@@ -9,48 +9,62 @@ import java.util.Collections;
 
 public class BruteForce
 {
-	//returnBoxes is de arrayList die boxen returned met de producten.
-	//latestBoxes is de laatste gesorteerde arraylist
+	// returnBoxes is de arrayList die boxen returned met de producten.
+	// latestBoxes is de laatste gesorteerde arraylist
 	public int boxVolume;
 	private BppSimulation simulation;
 	private ArrayList<Box> returnBoxes = new ArrayList<Box>();
-	public BruteForce(BppSimulation simulation){
-		this.simulation=simulation;
+
+	public BruteForce(BppSimulation simulation)
+	{
+		this.simulation = simulation;
 	}
-	public ArrayList<Box> executeBruteForce(ArrayList<Product> products) {
+
+	public ArrayList<Box> executeBruteForce(ArrayList<Product> products)
+	{
 		int currentBoxAmount;
-		int bestBoxAmount=products.size()+1;
-		for(int i =0;i<products.size();i++){
-			currentBoxAmount=sortProducts(products).size();
-			if(currentBoxAmount<bestBoxAmount)){
-				bestBoxAmount=currentBoxAmount;
-				simulation.addConsoleItem("New best amount","DEBUG");
+		int bestBoxAmount = products.size() + 1;
+		for (int i = 0; i < products.size(); i++)
+		{
+			currentBoxAmount = sortProducts(products).size();
+			if (currentBoxAmount < bestBoxAmount)
+			{
+				bestBoxAmount = currentBoxAmount;
+				simulation.addConsoleItem("New best amount", "DEBUG");
 			}
-			else{
-				bestBoxAmount=currentBoxAmount;
+			else
+			{
+				bestBoxAmount = currentBoxAmount;
 			}
-			Collections.rotate(products,1);
+			Collections.rotate(products, 1);
 		}
-		simulation.addConsoleItem("Best Box amount = "+bestBoxAmount,"DEBUG");
-		simulation.addConsoleItem("FINISHED","INFO");
+		simulation.addConsoleItem("Best Box amount = " + bestBoxAmount, "DEBUG");
+		simulation.addConsoleItem("FINISHED", "INFO");
 		return returnBoxes;
 	}
-	private ArrayList<Box> sortProducts(ArrayList<Product> products) {
+
+	private ArrayList<Box> sortProducts(ArrayList<Product> products)
+	{
 		ArrayList<Box> sortedBoxes = new ArrayList<>();
-		boolean fitted=false;
-		for (Product product : products) {
-			for (Box currentBox : sortedBoxes) {
-				if (currentBox.checkFit(product.GetSize()) && fitted == false) {
+		boolean fitted = false;
+		for (Product product : products)
+		{
+			for (Box currentBox : sortedBoxes)
+			{
+				if (currentBox.checkFit(product.GetSize()) && fitted == false)
+				{
 					simulation.addConsoleItem("Product fitted", "DEBUG");
 					currentBox.addProduct(product);
 					fitted = true;
 				}
 			}
-			if (fitted == false) {
+			if (fitted == false)
+			{
 				Box newBox = new Box(boxVolume);
 				sortedBoxes.add(newBox);
 				newBox.addProduct(product);
-				simulation.addConsoleItem("Product did not fit, new box created. Amount of boxes is: " + sortedBoxes.size(), "DEBUG");
+				simulation.addConsoleItem(
+						"Product did not fit, new box created. Amount of boxes is: " + sortedBoxes.size(), "DEBUG");
 			}
 			fitted = false;
 		}
