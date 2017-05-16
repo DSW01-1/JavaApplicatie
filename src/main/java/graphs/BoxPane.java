0b809a98b9fde2b1828fd5f0901819b0c370ebc8
foepje12 <jellevanommen@hotmail.nl>
2017-05-16 20:28:44 +0200

    no message

--- src/main/java/graphs/BoxPane.java
+++ src/main/java/graphs/BoxPane.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.HBox;
+import main.java.constant.Constants;
 import main.java.main.ScreenProperties;
 import main.java.main.Vector2;
 import main.java.pane.base.StyledPane;
@@ -11,8 +12,10 @@ import main.java.pane.base.StyledScrollPane;
 
 public class BoxPane extends StyledPane
 {
+	private int boxPaneSize = Constants.gridSize;
 	private StyledScrollPane scrollPane;
 	private HBox hBox;
+
 	public BoxPane()
 	{
 		super();
@@ -22,25 +25,23 @@ public class BoxPane extends StyledPane
 
 	private void AddScrollPane()
 	{
-
-		int scrnWidth = ScreenProperties.getScreenWidth();
-		int scrnHeight = ScreenProperties.getScreenHeight();
-
 		hBox = new HBox();
-		hBox.setPrefHeight(scrnHeight / 3);
+		hBox.setPrefHeight(boxPaneSize);
 
-		scrollPane = new StyledScrollPane(hBox, new Vector2(scrnHeight / 3, scrnHeight / 3),
-				new Vector2(scrnWidth / 2 - scrnHeight / 3, 15));
+		scrollPane = new StyledScrollPane(hBox, new Vector2(boxPaneSize, boxPaneSize),
+				new Vector2((ScreenProperties.getScreenWidth() / 2) - boxPaneSize / 2, 15));
 
 		scrollPane.setVbarPolicy(ScrollBarPolicy.NEVER);
 		scrollPane.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
 		getChildren().add(scrollPane);
 	}
-	private void ResetScrollPane(){
+
+	public void AddBoxList(ArrayList<Box> boxArrayList)
+	{
 		hBox.getChildren().removeAll();
-	}
-	private void AddBoxList(ArrayList<Box> boxArrayList){
-		for (Box box : boxArrayList){
+
+		for (Box box : boxArrayList)
+		{
 			hBox.getChildren().add(new BoxCanvas(box));
 		}
 
--- src/main/java/pane/simulation/TspSimulation.java
+++ src/main/java/pane/simulation/TspSimulation.java
@@ -34,10 +34,10 @@ public class TspSimulation extends BaseSimulation
 	{
 		consolePane = new ConsolePane();
 		getChildren().add(consolePane);
-
 	}
 
-	private void AddProgressBar(){
+	private void AddProgressBar()
+	{
 		this.progression = new ProgressBar(0);
 		progression.setLayoutX(15);
 		progression.setLayoutY(550);
@@ -80,7 +80,8 @@ public class TspSimulation extends BaseSimulation
 		AddGrid(newSize);
 	}
 
-	public void updatePath(ArrayList<Vector2> shortestPath){
+	public void updatePath(ArrayList<Vector2> shortestPath)
+	{
 		grid.Redraw(shortestPath);
 	}
 
@@ -114,7 +115,7 @@ public class TspSimulation extends BaseSimulation
 			for (Vector2 coordinate : shortestPath)
 			{
 				coordinates += String.format("(%s, %s) ", coordinate.getX(), coordinate.getY());
-				addConsoleItem(String.format("Tile: x=%s, y=%s",coordinate.getX(), coordinate.getY()),"INFO");
+				addConsoleItem(String.format("Tile: x=%s, y=%s", coordinate.getX(), coordinate.getY()), "INFO");
 			}
 			addConsoleItem(coordinates, "INFO");
 			long duration = (stopTime - startTime) / 100000;
@@ -144,35 +145,40 @@ public class TspSimulation extends BaseSimulation
 		addConsoleItem("Searching for Coordinates", "DEBUG");
 
 		// ArrayList<GridTile> activeTiles = newGrid.getSelectedTiles();
-		if (activeTiles.size() > 0) {
+		if (activeTiles.size() > 0)
+		{
 
 			// STAP 1
-			//long startTime = System.nanoTime();
+			// long startTime = System.nanoTime();
 			addConsoleItem("STEP 1/3 | CALCULATING ALL PATHS", "DEBUG");
-			//TotalEnumeration algoritme = new TotalEnumeration();
+			// TotalEnumeration algoritme = new TotalEnumeration();
 
-			TotalEnumeration algoritme = new TotalEnumeration(activeTiles,this);
+			TotalEnumeration algoritme = new TotalEnumeration(activeTiles, this);
 			algoritme.start();
 
 			addConsoleItem("blabla", "DEBUG");
 
-			if(algoritme.getState() == Thread.State.TERMINATED){
+			if (algoritme.getState() == Thread.State.TERMINATED)
+			{
 				addConsoleItem("There are " + algoritme.getPossiblePaths() + " possible paths", "DEBUG");
-				//long stopTime = System.nanoTime();
-				//long duration = (stopTime - startTime) / 100000;
-				//String showDuration = (duration < 1) ? "duration: less then a ms" : "duration: " + duration + " ms, moving to the next step";
-				//addConsoleItem(showDuration, "INFO");
+				// long stopTime = System.nanoTime();
+				// long duration = (stopTime - startTime) / 100000;
+				// String showDuration = (duration < 1) ? "duration: less then a
+				// ms" : "duration: " + duration + " ms, moving to the next
+				// step";
+				// addConsoleItem(showDuration, "INFO");
 			}
 
 			// stap 2
-			//ArrayList<Vector2> shortestPath = algoritme.getShortestPath();
+			// ArrayList<Vector2> shortestPath = algoritme.getShortestPath();
 
-			//grid.Redraw(shortestPath);
+			// grid.Redraw(shortestPath);
 
-
-
-		}else{
-			addConsoleItem(String.format("%s coordinates found, algorithm has been cancelled", activeTiles.size()),"DEBUG");
+		}
+		else
+		{
+			addConsoleItem(String.format("%s coordinates found, algorithm has been cancelled", activeTiles.size()),
+					"DEBUG");
 		}
 	}
 
@@ -187,10 +193,9 @@ public class TspSimulation extends BaseSimulation
 		consolePane.getItems().add(consolePane.getItems().size(), String.format("[%s] %s", msgType, message));
 	}
 
-	public void changeProgression(int progress){
+	public void changeProgression(int progress)
+	{
 		progression.setProgress(progress);
 	}
-
-
 
 }
