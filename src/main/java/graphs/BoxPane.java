855ce3fdbffe7e3297617346e45d958ce0e61bfc
Martijn Compagner <martijn.compagner@live.nl>
2017-05-16 15:03:56 +0200

    box fix 1.1

--- src/main/java/graphs/BoxPane.java
+++ src/main/java/graphs/BoxPane.java
@@ -11,11 +11,13 @@ import main.java.pane.base.StyledScrollPane;
 
 public class BoxPane extends StyledPane
 {
-
+	private StyledScrollPane scrollPane;
+	private HBox hBox;
 	public BoxPane()
 	{
 		super();
 		AddScrollPane();
+
 	}
 
 	private void AddScrollPane()
@@ -24,20 +26,23 @@ public class BoxPane extends StyledPane
 		int scrnWidth = ScreenProperties.getScreenWidth();
 		int scrnHeight = ScreenProperties.getScreenHeight();
 
-		HBox hBox = new HBox();
+		hBox = new HBox();
 		hBox.setPrefHeight(scrnHeight / 3);
-		// ArrayList<Box> boxArray = new ArrayList<Box>();
 
-		Product[] productArray =
-		{ new Product(2), new Product(2), new Product(4) };
-
-		hBox.getChildren().add(new BoxCanvas(new Box(productArray)));
-
-		StyledScrollPane scrollPane = new StyledScrollPane(hBox, new Vector2(scrnHeight / 3, scrnHeight / 3),
+		scrollPane = new StyledScrollPane(hBox, new Vector2(scrnHeight / 3, scrnHeight / 3),
 				new Vector2(scrnWidth / 2 - scrnHeight / 3, 15));
 
 		scrollPane.setVbarPolicy(ScrollBarPolicy.NEVER);
 		scrollPane.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
 		getChildren().add(scrollPane);
+	}
+	private void ResetScrollPane(){
+		hBox.getChildren().removeAll();
+	}
+	private void AddBoxList(ArrayList<Box> boxArrayList){
+		for (Box box : boxArrayList){
+			hBox.getChildren().add(new BoxCanvas(box));
+		}
+
 	}
 }
